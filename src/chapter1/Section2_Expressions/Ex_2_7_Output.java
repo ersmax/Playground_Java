6c0fa3dc679fbb9b64297fa0fe0f21ad545584df
ersmax <e.masarin@student.vu.nl>
2025-09-19 16:24:40 +0200

    Add output

---
+++ src/chapter1/Section2_Expressions/Ex_2_7_Output.java
@@ -0,0 +1,24 @@
+package chapter1.Section2_Expressions;
+/*
+What is the output produced by the following lines of program code?
+char a, b;
+a = 'b';
+System.out.println(a);
+b = 'c';
+System.out.println(b);
+a = b;
+System.out.println(a);
+ */
+
+
+public class Ex_2_7_Output {
+    public static void main(String[] args) {
+        char a, b;
+        a = 'b';
+        System.out.println(a);  // b
+        b = 'c';
+        System.out.println(b);  // c
+        a = b;
+        System.out.println(a);  // c
+    }
+}
