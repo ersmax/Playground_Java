f8ebce9387b3f38811b44cb740568273ad7c5ff0
ersmax <e.masarin@student.vu.nl>
2025-09-19 16:07:02 +0200

    Add variables

---
+++ src/chapter1/Section2_Expressions/Ex_2_1_Legit_name.java
@@ -0,0 +1,15 @@
+package chapter1.Section2_Expressions;
+/*
+Which of the following may be used as variable names in Java?
+rate1, 1stPlayer, myprogram.java, long,
+TimeLimit, numberOfWindows
+ */
+
+public class Ex_2_1_Legit_name {
+    // The following may be used as variable names in Java:
+    // rate1, TimeLimit, numberOfWindows
+    // The following may NOT be used as variable names in Java:
+    // 1stPlayer (cannot start with a digit),
+    // myprogram.java (cannot contain a period),
+    // long (reserved keyword)
+}
---
+++ src/chapter1/Section2_Expressions/Ex_2_2_Var_names.java
@@ -0,0 +1,7 @@
+package chapter1.Section2_Expressions;
+
+public class Ex_2_2_Var_names {
+    // Can a Java program have two different variables named number and Number?
+    // Yes, a Java program can have two different variables named number and Number.
+    int number, Number;
+}
