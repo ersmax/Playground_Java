1d819143c77bce69866f420e31a330e473ae9477
ersmax <e.masarin@student.vu.nl>
2025-09-22 20:04:06 +0200

    Add operation 3

---
+++ src/chapter1/Section2_Expressions/Ex_2_20_Output_3.java
@@ -0,0 +1,9 @@
+package chapter1.Section2_Expressions;
+
+public class Ex_2_20_Output_3 {
+    public static void main(String[] args) {
+        int result = 11;
+        result /= 2;
+        System.out.println("result: " + result); // result: 5
+    }
+}
