1bce3434d9403ff9d71d27feb13ce9a2854e5650
ersmax <e.masarin@student.vu.nl>
2025-09-22 19:57:36 +0200

    Add operation

---
+++ src/chapter1/Section2_Expressions/Ex_2_18_Ouput.java
@@ -0,0 +1,15 @@
+/*
+What is the output of the following program lines?
+double number = (1/3) * 3;
+System.out.println("(1/3) * 3 is equal to " + number);
+ */
+
+
+package chapter1.Section2_Expressions;
+
+public class Ex_2_18_Ouput {
+    public static void main(String[] args) {
+        double number = (1/3) * 3;  // Output: (1/3) * 3 is equal to 0.0
+        System.out.println("(1/3) * 3 is equal to " + number);
+    }
+}
