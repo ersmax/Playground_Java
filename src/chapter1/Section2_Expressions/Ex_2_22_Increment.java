0fb7ddb83ee0b8bed46c6cc984a0152dd91c9ec0
ersmax <e.masarin@student.vu.nl>
2025-09-24 20:43:51 +0200

    Add increment

---
+++ src/chapter1/Section2_Expressions/Ex_2_22_Increment.java
@@ -0,0 +1,14 @@
+/*
+What is the output produced by the following lines of program code?
+int n = (int)3.9;
+System.out.println("n == " + n);
+ */
+
+package chapter1.Section2_Expressions;
+
+public class Ex_2_22_Increment {
+    public static void main(String[] args) {
+        int n = (int)3.9;
+        System.out.println("n == " + n); // n == 3
+    }
+}
