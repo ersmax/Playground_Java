b4128e27d2b32a8c9a7a13d33b4f7d8f97755e6a
ersmax <e.masarin@student.vu.nl>
2025-09-25 18:59:35 +0200

    Add string output

---
+++ src/chapter1/Section3_String/Ex_2_24_String_output.java
@@ -0,0 +1,14 @@
+/*
+What is the output produced by the following?
+String verbPhrase = "is money";
+System.out.println("Time" + verbPhrase);
+ */
+
+package chapter1.Section3_String;
+
+public class Ex_2_24_String_output {
+    public static void main(String[] args) {
+        String verbPhrase = "is money";
+        System.out.println("Time " + verbPhrase); // Time is money
+    }
+}
