9a8775b77da26676461059caf6fd57788afd52bb
ersmax <e.masarin@student.vu.nl>
2025-09-25 19:24:25 +0200

    Add String output5

---
+++ src/chapter1/Section3_String/Ex_2_28_String_output_5.java
@@ -0,0 +1,13 @@
+/*
+What is the output produced by the following?
+System.out.println("abc\\ndef");
+ */
+
+
+package chapter1.Section3_String;
+
+public class Ex_2_28_String_output_5 {
+    public static void main(String[] args) {
+        System.out.println("abc\\ndef"); // abc\ndef
+    }
+}
