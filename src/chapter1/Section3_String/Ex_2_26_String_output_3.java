b2cb00713d235a08a139980165d05f35dcdeab6e
ersmax <e.masarin@student.vu.nl>
2025-09-25 19:21:21 +0200

    Add String output3

---
+++ src/chapter1/Section3_String/Ex_2_26_String_output_3.java
@@ -0,0 +1,14 @@
+/*
+What is the output produced by the following?
+String test = "abcdefg";
+System.out.println(test.substring(3));
+ */
+
+package chapter1.Section3_String;
+
+public class Ex_2_26_String_output_3 {
+    public static void main(String[] args) {
+        String test = "abcdefg";
+        System.out.println(test.substring(3)); // defg
+    }
+}
