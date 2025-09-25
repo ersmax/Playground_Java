f1a62327ac1895d4cf4f7d4c105e509a5a6010c2
ersmax <e.masarin@student.vu.nl>
2025-09-25 19:57:53 +0200

    Add interest and balance

---
+++ src/chapter1/Section3_String/Ex_2_0_2_Interests.java
@@ -0,0 +1,21 @@
+package chapter1.Section3_String;
+
+public class Ex_2_0_2_Interests {
+    /**
+     Program that shows interest on a sample account balance
+    */
+    public static final double INTEREST_RATE = 2.5;
+
+    public static void main(String[] args) {
+        double balance = 100.0;
+        double interest;
+
+        interest = balance * (INTEREST_RATE / 100.0);
+        System.out.println("On a balance of $"
+                + balance + "\n"
+                + "you will earn interest of $"
+                + interest + "\n"
+                + "all in one short year.");
+    }
+
+}
