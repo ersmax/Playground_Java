9714861a1a1c4c59b08b64766d6ef59c1952a44e
ersmax <e.masarin@student.vu.nl>
2025-09-25 19:25:25 +0200

    Add String output6

---
+++ src/chapter1/Section3_String/Ex_2_29_String_output_6.java
@@ -0,0 +1,15 @@
+/*
+What is the output produced by the following?
+String test = "Hello Tony";
+test = test.toUpperCase();
+System.out.println(test);
+ */
+package chapter1.Section3_String;
+
+public class Ex_2_29_String_output_6 {
+    public static void main(String[] args) {
+        String test = "Hello Tony";
+        test = test.toUpperCase();
+        System.out.println(test); // HELLO TONY
+    }
+}
