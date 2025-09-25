0593202059f7664a6fdcfe31e1c2b47e62acbfe2
ersmax <e.masarin@student.vu.nl>
2025-09-25 19:09:53 +0200

    Add String output

---
+++ src/chapter1/Section3_String/Ex_2_25_Output.java
@@ -0,0 +1,15 @@
+/*
+What is the output produced by the following?
+String test = "abcdefg";
+System.out.println(test.length());
+System.out.println(test.charAt(1));
+ */
+package chapter1.Section3_String;
+
+public class Ex_2_25_Output {
+    public static void main(String[] args) {
+        String test = "abcdefg";
+        System.out.println(test.length()); // 7
+        System.out.println(test.charAt(1)); // b
+    }
+}
