932e16c586d4e2bb8bb72dcdd42360e28a9273c4
ersmax <e.masarin@student.vu.nl>
2025-09-25 19:44:29 +0200

    Fix class Person

--- src/chapter1/Section3_String/Ex_2_31_Increase_age.java
+++ src/chapter1/Section3_String/Ex_2_31_Increase_age.java
@@ -9,24 +9,28 @@ change the data in sam so that it simulates sam aging by 10 years.
 
 package chapter1.Section3_String;
 
-class Person {
-    private int age;
-
-    public Person() {
-        this.age = 0; // Initial age
-    }
-
-    public void increaseAge(int years) {
-        if (years > 0) {
-            this.age += years;
-            System.out.println("Age increased by " + years + " years. New age: " + this.age);
-        } else {
-            System.out.println("Invalid number of years to increase.");
-        }
-    }
-}
 
 public class Ex_2_31_Increase_age {
+
+    static class Person {
+        private int age;
+
+        public Person() {
+            this.age = 0; // Initial age
+        }
+
+        public void increaseAge(int years) {
+            if (years > 0) {
+                this.age += years;
+                System.out.println("Age increased by " + years + " years. New age: " + this.age);
+            } else {
+                System.out.println("Invalid number of years to increase.");
+            }
+        }
+    }
+
+
+
     public static void main(String[] args) {
         Person sam = new Person();
         sam.increaseAge(10);
