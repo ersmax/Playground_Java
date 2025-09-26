c135174623b1651cc67e976b0576c1f603561b84
ersmax <e.masarin@student.vu.nl>
2025-09-26 12:11:15 +0200

    Add constant

---
+++ src/chapter1/Section3_String/Ex_2_35_Spelling_convention.java
@@ -0,0 +1,15 @@
+/*
+What is the normal spelling convention for named constants?
+ */
+
+// ALLE LETTERS IN UPPERCASE, AND WORDS ARE SEPARATED BY UNDERSCORES _
+// In addition, the constant is declared outside any method, usually at the beginning of the class,
+// and is marked as static and final to indicate that its value cannot be changed.
+
+package chapter1.Section3_String;
+
+public class Ex_2_35_Spelling_convention {
+    public static final int MAX_VALUE = 100;
+    public static void main(String[] args) {
+    }
+}
---
+++ src/chapter1/Section3_String/Ex_2_36_Constant.java
@@ -0,0 +1,15 @@
+/*
+Write a line of Java code that will give the name ANSWER to the int value 42.
+In other words, make ANSWER a named constant for 42.
+ */
+
+package chapter1.Section3_String;
+
+public class Ex_2_36_Constant {
+    public static final int ANSWER = 42;
+
+    public static void main(String[] args) {
+        System.out.println("The answer is: " + ANSWER);
+    }
+
+}
