d18f3f215895f9e77167cdcff73c29eb824de0da
ersmax <e.masarin@student.vu.nl>
2025-09-26 11:44:25 +0200

    Add Comment 2

---
+++ src/chapter1/Section3_String/Ex_2_34_Comment_output.java
@@ -0,0 +1,22 @@
+/*
+What is the output produced by the following Java code?
+/**
+Code for Exercise.
+
+System.out.println("Hello");
+//System.out.print("Mr. or Ms. ");
+        System.out.println("Student");
+ */
+
+// Hello
+// Student
+
+package chapter1.Section3_String;
+
+public class Ex_2_34_Comment_output {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        //System.out.print("Mr. or Ms. ");
+        System.out.println("Student");
+    }
+}
