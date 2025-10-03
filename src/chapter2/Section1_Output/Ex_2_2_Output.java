843b3bec33e1de4c829a35ac4fb3a1f7e53611dc
ersmax <e.masarin@student.vu.nl>
2025-10-03 21:53:20 +0200

    Add output2

---
+++ src/chapter2/Section1_Output/Ex_2_2_Output.java
@@ -0,0 +1,14 @@
+package chapter2.Section1_Output;
+
+/*
+Write Java statements that will cause the following to be written to the screen:
+One two buckle your shoe.
+Three four shut the door.
+ */
+
+public class Ex_2_2_Output {
+    public static void main(String[] args) {
+        System.out.print("One two buckle your shoe.\n");
+        System.out.println("Three four shut the door.");
+    }
+}
