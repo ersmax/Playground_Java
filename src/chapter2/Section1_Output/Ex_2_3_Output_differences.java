90c2c0bd8036ce6db5d07130f12072a4e6300cf8
ersmax <e.masarin@student.vu.nl>
2025-10-03 21:57:23 +0200

    Add output3

---
+++ src/chapter2/Section1_Output/Ex_2_3_Output_differences.java
@@ -0,0 +1,12 @@
+/*
+What is the difference between System.out.println and System.out.print?
+ */
+
+// Answer: System.out.println outputs the text followed by a newline character, \n,
+// moving the cursor to the next line,
+// while System.out.print outputs the text without adding a newline,
+// keeping the cursor on the same line.
+package chapter2.Section1_Output;
+
+public class Ex_2_3_Output_differences {
+}
