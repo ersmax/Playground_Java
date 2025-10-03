f364b4130e3fdfe98d55132324de136e3a0f6210
ersmax <e.masarin@student.vu.nl>
2025-10-03 21:58:35 +0200

    Add output4

---
+++ src/chapter2/Section1_Output/Ex_2_4_Output.java
@@ -0,0 +1,18 @@
+/*
+What is the output produced by the following lines?
+System.out.println(2 + " " + 2);
+System.out.println(2 + 2);
+ */
+
+// Answer:
+// 2 2
+// 4
+
+package chapter2.Section1_Output;
+
+public class Ex_2_4_Output {
+    public static void main(String[] args) {
+        System.out.println(2 + " " + 2);
+        System.out.println(2 + 2);
+    }
+}
