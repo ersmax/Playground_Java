a74a1097baded1fcf8a8ea38cfbb8af7a35e8d91
ersmax <e.masarin@student.vu.nl>
2025-10-03 21:51:57 +0200

    Add output

---
+++ src/chapter2/Section1_Output/Ex_2_1_Output.java
@@ -0,0 +1,16 @@
+/*
+What output is produced by the following lines?
+String s = "Hello" + "Joe";
+System.out.println(s);
+ */
+
+// Answer: HelloJoe
+
+package chapter2.Section1_Output;
+
+public class Ex_2_1_Output {
+    public static void main(String[] args) {
+        String s = "Hello" + "Joe";
+        System.out.println(s);
+    }
+}
