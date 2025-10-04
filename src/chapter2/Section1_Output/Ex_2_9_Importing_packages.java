72a1ac7de6f8c778125ff84331778e4723d5a6c9
ersmax <e.masarin@student.vu.nl>
2025-10-04 15:40:56 +0200

    Add ex import packages

---
+++ src/chapter2/Section1_Output/Ex_2_9_Importing_packages.java
@@ -0,0 +1,15 @@
+package chapter2.Section1_Output;
+
+/*
+Suppose the class Robot is a part of the standard Java libraries and is in the
+package named java.awt. What import statement do you need to make the
+class Robot available to your program or other class?
+ */
+
+import java.awt.Robot;
+
+public class Ex_2_9_Importing_packages {
+    public static void main(String[] args) {
+
+    }
+}
