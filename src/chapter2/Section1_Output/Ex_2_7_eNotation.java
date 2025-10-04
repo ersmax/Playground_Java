7cae41f5a0742e72ce5f760e0b2931d02721611e
ersmax <e.masarin@student.vu.nl>
2025-10-04 14:33:49 +0200

    Add e notation format specifier

---
+++ src/chapter2/Section1_Output/Ex_2_7_eNotation.java
@@ -0,0 +1,12 @@
+package chapter2.Section1_Output;
+/*
+Write a Java statement to output the value in variable d of type double to the
+screen. The output should be in e-notation with three digits after the decimal
+point. The output should be in a field of width 15.
+ */
+public class Ex_2_7_eNotation {
+    public static void main(String[] args) {
+        double d = 3212.412312213;
+        System.out.printf("%15.3e", d);
+    }
+}
