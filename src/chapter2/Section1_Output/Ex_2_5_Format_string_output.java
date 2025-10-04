57b5748913a723ab4bad50793e04c243a7893460
ersmax <e.masarin@student.vu.nl>
2025-10-04 14:20:14 +0200

    Add format specifier Jelly Beans

---
+++ src/chapter2/Section1_Output/Ex_2_5_Format_string_output.java
@@ -0,0 +1,22 @@
+/*
+What output is produced by the following code?
+String aString = "Jelly beans";
+System.out.println("START1234567890");
+System.out.printf("START%sEND %n", aString);
+System.out.printf("START%4sEND %n", aString);
+System.out.printf("START%13sEND %n", aString);
+System.out.println();
+ */
+
+package chapter2.Section1_Output;
+
+public class Ex_2_5_Format_string_output {
+    public static void main(String[] args) {
+        String aString = "Jelly beans";
+        System.out.println("START1234567890");
+        System.out.printf("START%sEND %n", aString); // STARTJelly beansEND /n
+        System.out.printf("START%4sEND %n", aString); // STARTJelly beansEND /n
+        System.out.printf("START%13sEND %n", aString); // START  Jelly beansEND /n
+        System.out.println();
+    }
+}
