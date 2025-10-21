3f918c395b5b9fbabf4525b7969a8db62b31c397
ersmax <e.masarin@student.vu.nl>
2025-10-21 13:05:03 +0200

    Add 2.10 and 2.11

---
+++ src/chapter2/Section2_Input_Scanner/Ex_2_10_Scanner.java
@@ -0,0 +1,12 @@
+package chapter2.Section2_Input_Scanner;
+/*
+Write an import statement that makes the Scanner class available to your
+program or other class.
+ */
+
+import java.util.Scanner;
+
+public class Ex_2_10_Scanner {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/chapter2/Section2_Input_Scanner/Ex_2_10_Scanner_p2.java
@@ -0,0 +1,12 @@
+package chapter2.Section2_Input_Scanner;
+/*
+Write a line of code that creates a Scanner object named frank to be used for
+obtaining keyboard input.
+ */
+import java.util.Scanner;
+
+public class Ex_2_10_Scanner_p2 {
+    public static void main(String[] args){
+        Scanner frank = new Scanner(System.in);
+    }
+}
