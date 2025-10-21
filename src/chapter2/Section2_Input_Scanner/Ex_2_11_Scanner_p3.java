8e16bc88c08f79e0d47cfa914b87592cde5abe1e
ersmax <e.masarin@student.vu.nl>
2025-10-21 13:07:03 +0200

    Add 2.13

---
+++ src/chapter2/Section2_Input_Scanner/Ex_2_11_Scanner_p3.java
@@ -0,0 +1,17 @@
+package chapter2.Section2_Input_Scanner;
+
+/*
+Write a line of code that uses the object frank from the previous exercise to
+read in a word from the keyboard and store the word in the String variable
+named w.
+ */
+
+import java.util.Scanner;
+
+public class Ex_2_11_Scanner_p3 {
+    public static void main(String[] args) {
+        Scanner frank = new Scanner(System.in);
+        String w;
+        w = frank.next();
+    }
+}
