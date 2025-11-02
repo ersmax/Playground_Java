3990e8c2b79d529e2e0870abb43b66e1dd716d6a
ersmax <e.masarin@student.vu.nl>
2025-11-02 23:47:45 +0100

    Add comparing string

---
+++ src/chapter3/Section2_Boolean/Ex_3_0_4_Comparing_String.java
@@ -0,0 +1,33 @@
+package chapter3.Section2_Boolean;
+
+public class Ex_3_0_4_Comparing_String {
+    public static void main(String[] args) {
+        String s1 = "Java isn't just for breakfast";
+        String s2 = "JAVA isn't just for breakfast";
+
+        if (s1.equals(s2))
+            System.out.println("The two lines are equal.");
+        else
+            System.out.println("The two lines are not equal.");
+
+        if (s2.equals(s1))
+            System.out.println("The two lines are equal.");
+        else
+            System.out.println("The two lines are not equal.");
+
+        if (s1.equalsIgnoreCase(s2))
+            System.out.println(
+            "But the lines are equal, ignoring case.");
+        else
+            System.out.println(
+            "Line are not equal, even ignoring case.");
+
+        String s3 = "A cup of java is a joy forever.";
+        if (s3.compareToIgnoreCase(s1) < 0) {
+            System.out.println("\"" + s3 + "\"");
+            System.out.println("precedes");
+            System.out.println("\"" + s1 + "\"");
+        } else
+            System.out.println("s3 does not precede s1");
+    }
+}
