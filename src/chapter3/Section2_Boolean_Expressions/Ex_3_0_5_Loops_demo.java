947530c5c1a4ed60b408b064df3b2f2f73e662ea
ersmax <masarin.eros@gmail.com>
2026-05-08 19:45:34 +0200

    Adding avg and loop demo

---
+++ src/chapter3/Section2_Boolean_Expressions/Ex_3_0_5_Loops_demo.java
@@ -0,0 +1,35 @@
+package chapter3.Section2_Boolean_Expressions;
+
+public class Ex_3_0_5_Loops_demo {
+    public static void main(String[] args)  {
+        int countDown;
+        System.out.println("First while loop:");
+        countDown = 3;
+        while (countDown > 0) {
+            System.out.println("Hello");
+            countDown -= 1;
+        }
+        System.out.println("Second while loop:");
+        countDown = 0;
+        while (countDown > 0) {
+            System.out.println("Hello");
+            countDown -= 1;
+
+        }
+        System.out.println("First do-while loop:");
+        countDown = 3;
+        do {
+            System.out.println("Hello");
+            countDown -= 1;
+        } while (countDown > 0);
+
+        System.out.println("Second do-while loop:");
+        countDown = 0;
+        do {
+            System.out.println("Hello");
+            countDown -= 1;
+        } while (countDown > 0);
+
+        System.out.println(countDown);
+    }
+}
---
+++ src/chapter3/Section2_Boolean_Expressions/Ex_3_0_6_Average.java
@@ -0,0 +1,28 @@
+package chapter3.Section2_Boolean_Expressions;
+
+import java.util.Scanner;
+
+public class Ex_3_0_6_Average {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a list of nonnegative scores.");
+        System.out.println("Mark the end with a negative number");
+        System.out.println("I will compute the average");
+
+        double next, sum = 0;
+        int count = 0;
+        next = keyboard.nextDouble();
+        while (next >= 0) {
+            sum += next;
+            count++;
+            next = keyboard.nextDouble();
+        }
+        if (count == 0)
+            System.out.println("No scores entered");
+        else {
+            double average = sum / count;
+            System.out.println(count + " scores read.");
+            System.out.println("The average is " + average);
+        }
+    }
+}
