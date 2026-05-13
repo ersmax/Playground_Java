c8883b33a6e1836bbea4175131fd29308fe77fab
ersmax <masarin.eros@gmail.com>
2026-05-13 10:31:58 +0200

    Change assertion

--- src/chapter3/Section4_Debugging/Ex_3_43_Assertion.java
+++ src/chapter3/Section4_Debugging/Ex_3_43_Assertion.java
@@ -1,4 +1,16 @@
+/*
+Write an assertion check that checks to see that the value of the variable time is
+less than or equal to the value of the variable limit. Both variables are of type int.
+Generating
+Random
+ */
+
 package chapter3.Section4_Debugging;
 
 public class Ex_3_43_Assertion {
+
+    public static void main(String[] args) {
+        int time = 10, limit = 100;
+        assert (time <= limit);
+    }
 }
