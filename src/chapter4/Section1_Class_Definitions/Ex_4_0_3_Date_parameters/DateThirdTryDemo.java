ea4d777df9e0976508296601844bbb7628fb57a4
ersmax <masarin.eros@gmail.com>
2026-05-13 16:03:45 +0200

    Add new date programs

---
+++ src/chapter4/Section1_Class_Definitions/Ex_4_0_3_Date_parameters/DateThirdTryDemo.java
@@ -0,0 +1,11 @@
+package chapter4.Section1_Class_Definitions.Ex_4_0_3_Date_parameters;
+
+public class DateThirdTryDemo {
+
+    public static void main(String[] args) {
+        DateThirdTry date = new DateThirdTry();
+        int year = 1900;
+        date.setDate(6, 30, 2026);
+        date.writeOutput();
+    }
+}
